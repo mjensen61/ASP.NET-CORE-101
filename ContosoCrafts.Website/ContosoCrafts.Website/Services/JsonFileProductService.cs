daccabe939f088605c70570a60a71f501ae56b37
mjensen61 <43187944+mjensen61@users.noreply.github.com>
2023-04-21 16:05:50 +1000

    WIP

--- ContosoCrafts.Website/ContosoCrafts.Website/Services/JsonFileProductService.cs
+++ ContosoCrafts.Website/ContosoCrafts.Website/Services/JsonFileProductService.cs
@@ -20,12 +20,16 @@ namespace ContosoCrafts.WebSite.Services
 
         public IEnumerable<Product> GetProducts()
         {
-            using var jsonFileReader = File.OpenText(JsonFileName);
-            return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
-                new JsonSerializerOptions
-                {
-                    PropertyNameCaseInsensitive = true
-                });
+            using (var jsonFileReader = File.OpenText(JsonFileName))
+            {
+#pragma warning disable CS8603 // Possible null reference return.
+                return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+#pragma warning restore CS8603 // Possible null reference return.
+            }
         }
 
         public void AddRating(string productId, int rating)
